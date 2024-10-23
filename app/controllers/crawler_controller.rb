48847f9e979866f6da54f36c0315a2dd20f8d95c
Marina Alane <marina.alane@outlook.com>
2024-10-23 14:28:04 -0300

    fix: crawler index visualization

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -3,8 +3,15 @@ require_relative "../helpers/crawler_helper.rb"
 class CrawlerController < ApplicationController
   def index
     @crawler_service = CrawlerService.new
-    @suppliers = @crawler_service.fetch_data(CrawlerHelper::DEPARTMENTS)
+    result = @crawler_service.fetch_data(CrawlerHelper::DEPARTMENTS)
 
-    render json: @suppliers
+    categories_list = []
+    @categories = result["departments"]
+
+    @categories.each do |category|
+      categories_list << { name: category["name"], id: category["id"] }
+    end
+
+   render json: categories_list
   end
 end
